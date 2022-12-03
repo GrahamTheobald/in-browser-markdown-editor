fdb0a28f447cdc1ba4910fe808908471789151ab
Graham Theobald <Graham_Theobald@live.co.uk>
2022-12-03 15:37:59 +0000

    text input when focused on file thumbnail

--- app/src/components/File.jsx
+++ app/src/components/File.jsx
@@ -1,11 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useState, useRef } from 'react'
 import { HandlerContext } from './App'
 import Icon from './utility/Icon'
 import documentIcon from '../assets/icon-document.svg'
 
 export default function File({ content }) {
-	const { handleActiveFile } = useContext(HandlerContext)
 	const { createdAt, name } = content
+	const [focused, setFocused] = useState(false)
+	const [newName, setNewName] = useState(name)
+	const { handleActiveFile } = useContext(HandlerContext)
+	const input = useRef()
 	return (
 		<div
 			onClick={() => handleActiveFile(content.id)}
@@ -16,7 +19,29 @@ export default function File({ content }) {
 				<p className='text-mid1 text-sm hidden md:block'>
 					{formatDate(createdAt)}
 				</p>
-				<p className='text-light1 text-sm break-all'>{name}</p>
+				{focused ? (
+					<form className='w-full h-full'>
+						<input
+							onChange={(e) => setNewName(e.target.value)}
+							autoFocus
+							spellCheck='false'
+							value={newName}
+							onBlur={() => setFocused(false)}
+							className='text-light1 text-sm break-all outline-none bg-mid1 rounded-sm h-6'
+						/>
+					</form>
+				) : (
+					<p
+						onFocus={() => {
+							setFocused(true)
+							input.current.focus()
+						}}
+						tabIndex={-1}
+						className='text-light1 text-sm break-all h-6 flex items-center'
+					>
+						{name}
+					</p>
+				)}
 			</div>
 		</div>
 	)
